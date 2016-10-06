af4549f067aff3e49c318494bf90efa140defc70
codeNgamer <ekeneoduah@live.com>
2016-10-06 17:14:04 -0500

    wip?

--- App/containers/flags/CulturalPage.js
+++ App/containers/flags/CulturalPage.js
@@ -1,14 +1,47 @@
 import React,{Component} from 'react';
 import {
-    Text,
-    View,
-    Image,
-    ScrollView,
-    StyleSheet,
-StatusBar
+  Text,
+  View,
+  Image,
+  ScrollView,
+  StyleSheet,
+  TouchableOpacity,
+  StatusBar
 } from 'react-native';
+import Cultures from './fraternities';
+import _ from 'lodash';
+import {
+  updateUserFlagAction
+} from '../../action-creators';
+import {Actions} from 'react-native-router-flux';
+import { connect } from 'react-redux';
 
 class CulturalPage extends  Component{
+  constructor(props){
+    super(props);
+    this._updateFlagData = _.bind(this._updateFlagData, this);
+  }
+  _updateFlagData({ picture, name }) {
+    const { flagIndex: index } = this.props;
+    const flag = { picture, name };
+    this.props.dispatchUpdateFlag({ flag, index });
+    Actions.pop();
+  }
+  _renderCultures() {
+    return _.map(_.sortBy(Cultures, 'name'), culture => {
+      const {flag: picture, name } = culture;
+      return (
+        <TouchableOpacity
+          key={name}
+          onPress={() => this._updateFlagData({ picture, name })}>
+        <View style={[styles.listItem]}>
+          <Text  style={[styles.listText]}>{name}</Text>
+          <Image source={{ uri: picture }} style={[styles.flag]} ></Image>
+        </View>
+      </TouchableOpacity>
+      )
+    });
+  }
   render(){
     return(
       <ScrollView style={{flex:1}}>
@@ -16,41 +49,44 @@ class CulturalPage extends  Component{
           hidden={false}
           barStyle="light-content"
         />
-        <View style={[styles.listItem]}><Text  style={[styles.listText]}>Alpha Kappa Alpha Soronity Inc.</Text></View>
-        <View style={[styles.listItem]}><Text style={[styles.listText]} selected={true}>Alpha Phi Alpha Fraternity Inc.</Text></View>
-        <View style={[styles.listItem]}><Text style={[styles.listText]}>Delta Sigma Theta Soronity Inc.</Text></View>
-        <View style={[styles.listItem]}><Text style={[styles.listText]}>Iota Phi Theta Fraternity Inc.</Text></View>
-        <View style={[styles.listItem]}><Text style={[styles.listText]}>Kappa Alpha Psi Fraternity Inc.</Text></View>
-        <View style={[styles.listItem]}><Text style={[styles.listText]}>Omega Psi Phi Fraternity Inc.</Text></View>
-        <View style={[styles.listItem]}><Text style={[styles.listText]}>Phi Beta Sigma Fraternity</Text></View>
-        <View style={[styles.listItem]}><Text style={[styles.listText]}>Sigma Gamma Rho Soronity</Text></View>
-        <View style={[styles.listItem]}><Text style={[styles.listText]}>Zeta Phi Beta Soronity Inc.</Text></View>
-        <View style={[styles.listItem]}><Text style={[styles.listText]}>Blerd</Text></View>
-        <View style={[styles.listItem]}><Text style={[styles.listText]}>HBCU Graduate</Text></View>
-        <View style={[styles.listItem]}><Text style={[styles.listText]}>Hip-Hop Lover</Text></View>
+        {this._renderCultures()}
       </ScrollView>
     )
   }
 }
 
 const styles = StyleSheet.create({
-    listItem:{
-        paddingTop:10,
-        paddingLeft:15,
-        paddingBottom:10,
-        paddingRight:45,
-        borderTopWidth:1,
-        borderColor:'#CCCCCC',
-        justifyContent:'space-between',
-        flexDirection:'row',
-        alignItems: 'center'
-    },
-    listText:{
-        fontSize:16
-    },
-    flag:{
-        width:45,
-        height:30
-    }
+  listItem:{
+    paddingTop:10,
+    paddingLeft:15,
+    paddingBottom:10,
+    paddingRight:45,
+    borderTopWidth:1,
+    borderColor:'#CCCCCC',
+    justifyContent:'space-between',
+    flexDirection:'row',
+    alignItems: 'center'
+  },
+  listText:{
+    fontSize:16
+  },
+  flag:{
+    width:45,
+    height:30
+  }
 })
-export default CulturalPage
+const mapStateToProps = state => {
+  return {
+    ...state,
+  }
+};
+const mapDispatchToProps = (dispatch, ownProps) => {
+  return {
+    dispatchUpdateFlag: flagInformation => dispatch(updateUserFlagAction(flagInformation)),
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(CulturalPage);
---
+++ App/containers/flags/fraternities.js
@@ -0,0 +1,50 @@
+export default [
+  {
+    name: 'Alpha Kappa Alpha Sorority Inc.',
+    flag: 'http://greeklife.oglethorpe.edu/files/2015/02/AKA-Logo-300x171.jpg',
+  },
+  {
+    name: 'Alpha Phi Alpha Fraternity Inc.',
+    flag: 'http://www.apa1906.net/Websites/alphaphialpha/images/GreekLetterLogoG.png',
+  },
+  {
+    name: 'Delta Sigma Theta Sorority Inc.',
+    flag: 'https://www.umflint.edu/sites/default/files/groups/Fraternity_and_Sorority_Life/Crests/gl_delta-sigma-theta.png',
+  },
+  {
+    name: 'Iota Phi Theta Fraternity Inc.',
+    flag: 'http://www.thepacer.net/wp-content/uploads/2014/08/Iota.png',
+  },
+  {
+    name: 'Kappa Alpha Psi Fraternity Inc.',
+    flag: 'http://rockymountainnupes.com/wp-content/uploads/2013/12/Kappa-Alpha-Psi1-370x247.png',
+  },
+  {
+    name: 'Omega Psi Phi Fraternity Inc.',
+    flag: 'http://www.thepacer.net/wp-content/uploads/2014/08/OmegaPsiPhi1.png',
+  },
+  {
+    name: 'Phi Beta Sigma Fraternity',
+    flag: 'http://kollegekidd.com/wp-content/uploads/2013/04/phi-beta-sigma.jpg',
+  },
+  {
+    name: 'Sigma Gamma Rho Sorority',
+    flag: 'http://stacysgotgreek.com/wordpress/wp-content/uploads/Sigma-Gamma-Rho-Flag.jpg',
+  },
+  {
+    name: 'Zeta Phi Beta Sorority Inc.',
+    flag: 'http://i1095.photobucket.com/albums/i471/mialove83/kamwk9.png',
+  },
+  {
+    name: 'Blerd',
+    flag: 'http://4.bp.blogspot.com/-NlhyTM9j-Y4/U-A5yjfXC2I/AAAAAAAAAiU/m5ezC_a-jkc/s1600/Blerd-Black-Nerd-graphic.jpg',
+  },
+  {
+    name: 'HBCU Graduate',
+    flag: 'http://az616578.vo.msecnd.net/files/2016/03/18/63593929738382622644307957_hbcu-colleges.jpg',
+  },
+  {
+    name: 'Hip-Hop Lover',
+    flag: 'https://ignaciopicazo.files.wordpress.com/2012/01/hip-hop2.jpg',
+  },
+];
